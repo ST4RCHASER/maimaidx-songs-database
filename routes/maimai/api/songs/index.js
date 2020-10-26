15a0ffe3bb54df118bcf0aa266ee3795ac1a355d
_StarChaser <32775520+meLStarChaser@users.noreply.github.com>
2020-10-26 12:59:55 +0700

    add title_kana to filter

--- routes/maimai/api/songs/index.js
+++ routes/maimai/api/songs/index.js
@@ -8,7 +8,8 @@ router.get('/', (req, res) => {
     const category = req.query.category;
     const artist = req.query.artist;
     const title = req.query.title;
-    songFinder(title,artist,category).forEach(item => {
+    const title_kana = req.query.title_kana;
+    songFinder(title,title_kana,artist,category).forEach(item => {
         songs.push(songObject(item.title));
     });
     return res.status(200).json({
